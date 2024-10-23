54c444929a76f19b758669980281adc19144ebc9
Chryssa Karatza <chryssakaratza@gmail.com>
2024-10-23 17:20:22 +0100

    initial commit

---
+++ chapter2/chapter2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("This sentence will be executed.\n");
+    exit(0);
+    printf("This sentence will never be executed.\n ");
+}
